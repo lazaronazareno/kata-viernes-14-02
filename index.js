b73173feb1c858757b7463c4076ac369a9d05c70
lazaronazareno <aragon.ezequiel98@gmail.com>
2020-02-14 13:55:24 -0300

    ejercicio de cambiar la primera letra de string a mayuscula

---
+++ index.js
@@ -0,0 +1,9 @@
+function mayus(){
+    let palabra="palabra";
+    let mayuscula = palabra.charAt(0).toUpperCase();
+
+    for(let i=0; i<mayuscula.length;i++){
+        console.log(mayuscula);
+
+    }
+}
